f133063c9992bf23ab1fab9f0377d1e110d5c1a4
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 23:39:36 -0700

    fix: maybe again idk

---
+++ scripts/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$DIR"
+
+runuser -u arcade -- env \
+    XDG_RUNTIME_DIR=/run/user/1001 \
+    WLR_BACKEND=drm \
+    WLR_RENDERER=gles2 \
+    GODOT_PLATFORM=wayland \
+    /usr/bin/cage -d -s -- ./builds/0.1/example-game.arm64
